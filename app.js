40ae6d08ecae39f70e579a32260b2d30e393b60f
rishabhurja <rishabh.indoria@urja.io>
2022-07-29 20:12:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+const items = ["Buy Grocery", "Chop Vegetables", "Cook Dinner"];
+const workItems = [];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+const day =date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+  app.post("/", function(req, res) {
+    const item = req.body.newItem;
+    if(req.body.list === "work"){
+      workItems.push(item);
+      res.redirect("/work");
+    }else{
+      items.push(item);
+      res.redirect("/");
+    }
+
+  });
+  app.get("/work", function(req, res) {
+    res.render("list", {
+      listTitle: "Work List",
+      newListItems: workItems
+    });
+  });
+
+  app.post("/work", function(req, res) {
+    const item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+  });
+
+});
+app.listen(3000, function() {
+  console.log("server is running on port 3000");
+});
